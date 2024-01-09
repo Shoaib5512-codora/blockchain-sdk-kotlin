d0370d135d60c4706b3b657c948172c84b4eb42e
Anton Tkachev <iiiburnyiii@gmail.com>
2024-01-09 17:06:59 +0300

    AND-5746 [Solana] Improved fee calculation logic

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/solana/solanaj/rpc/SolanaRpcClient.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/solana/solanaj/rpc/SolanaRpcClient.kt
@@ -6,12 +6,11 @@ import org.p2p.solanaj.rpc.RpcClient
 /**
  * Created by Anton Zhilenkov on 26/01/2022.
  */
-class RpcClient(
+internal class SolanaRpcClient(
     val host: String,
     httpInterceptors: List<Interceptor>? = null,
 ) : RpcClient(host, httpInterceptors) {
 
-    override fun createRpcApi(): RpcApi = RpcApi(this)
-
-    override fun getApi(): RpcApi = super.getApi() as RpcApi
+    override fun createRpcApi(): SolanaRpcApi = SolanaRpcApi(this)
+    override fun getApi(): SolanaRpcApi = super.getApi() as SolanaRpcApi
 }
